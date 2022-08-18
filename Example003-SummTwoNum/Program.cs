06f65dca7b40e1d352749f61d6bf09018ca02b36
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-18 17:54:56 +0300

    New project SumTwoNum added

---
+++ Example003-SummTwoNum/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Добро пожаловать в программу для сложения двух чисел!");
+Console.Write("Введите первое число:");
+int a=Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b=Int32.Parse(Console.ReadLine());
+int result=a+b;
+Console.Write("Результат сложения чисел:");
+Console.Write(result);
