acefeed521c8a45bc97e0f56dde838ddb21f25f0
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-19 22:22:15 +0300

    New project - Condition added Example002 folder renamed

---
+++ Example005-Condition/Program.cs
@@ -0,0 +1,10 @@
+﻿string name;
+
+Console.WriteLine("Welcome to the greeting app!");
+Console.Write("Please enter your name:");
+name = Console.ReadLine();
+if(name=="Mary")
+    Console.WriteLine("Dear Mary, I've been waiting for you years!");
+else
+    Console.WriteLine("Hello dear "+name+"!");
+
