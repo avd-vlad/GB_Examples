5149b78fef995b6da13cc26963b030ef0fe45939
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-19 21:34:13 +0300

    Project Div added

---
+++ Example004-Div/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Добро пожаловать в программу для деления двух чисел!");
+Console.Write("Введите первое число:");
+Double a=Double.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+Double b=Double.Parse(Console.ReadLine());
+Double result=0;
+
+
+result=a/b;
+
+Console.Write("Результат деления первого числа на второе:");
+Console.Write(result);
