e7ee89d0d747db692cdee70f74d272852bfc7376
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-20 01:03:35 +0300

    Fixed problem

--- Example005-Condition/Program.cs
+++ Example005-Condition/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Welcome to the greeting app!");
 Console.Write("Please enter your name:");
 name = Console.ReadLine();
-if(name=="Mary")
+if(name.ToUpper()=="MARY")
     Console.WriteLine("Dear Mary, I've been waiting for you years!");
 else
     Console.WriteLine("Hello dear "+name+"!");
