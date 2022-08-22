096eb337e749bb88935fff9984209e1acd822c54
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-22 23:39:28 +0300

    New examples added

---
+++ Example009-IntroArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int a, int b, int c)
+{
+    int res = a;
+
+    if (b > res) res = b;
+    if (c > res) res = c;
+
+    return res;
+}
+//              0   1   2   3   4   5   6   7   8
+int[] array ={ 11, 21, 31, 41, 15, 61, 17, 18, 19};
+
+int max = Max(Max(array[0],array[1],array[2]),
+              Max(array[3],array[4],array[5]),
+              Max(array[6],array[7],array[8])
+);
+
+Console.WriteLine(max);
---
+++ Example011-ArrayLib/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection)
+{
+    int len = collection.Length;
+    int ind = 0;
+
+    while(ind < len)
+    {
+        collection[ind] = new Random().Next(1,10);
+        ind++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int cnt = col.Length;
+    int pos = 0;
+
+    while(pos < cnt)
+    {
+        Console.WriteLine(col[pos]);
+        pos++;
+    }
+
+}
+
+
+int IndexOf(int[] col, int find)
+{
+    int cnt = col.Length;
+    int ind = 0;
+    int pos = -1;
+
+    while(ind<cnt)
+    {
+        if(col[ind]==find)
+        {
+            pos = ind;
+            break;
+        }
+        ind++;
+    }
+    return pos;
+}
+
+int[] arr = new int[10];
+
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+
+int pos = IndexOf(arr, 4);
+Console.WriteLine(pos);
